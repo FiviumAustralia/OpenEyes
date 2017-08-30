499c0f2d75e96e92b937b7380dc29006354eacb8
Andrew Pigram <andrew.pigram@fivium.com.au>
2017-08-30 18:01:09 +1000

    Added regular database to mysql script. Now using vendored phpunit to run tests. Fixed cd command prior to running tests.

--- mysql-ci.sh
+++ mysql-ci.sh
@@ -3,10 +3,8 @@
 echo "Adding openeyes user..."
 mysql -u root -Bse "CREATE USER '$DATABASE_TEST_USER'@'$DATABASE_TEST_HOST' IDENTIFIED BY '$DATABASE_TEST_PASS';
 CREATE DATABASE \`$DATABASE_TEST_NAME\`;
+CREATE DATABASE \`$DATABASE_NAME\`;
 COMMIT;
 GRANT ALL PRIVILEGES ON *.* TO '$DATABASE_TEST_USER'@'$DATABASE_TEST_HOST';
 FLUSH PRIVILEGES;"
-echo "Done."
-echo "Importing sample data..."
-mysql $DATABASE_TEST_NAME -u $DATABASE_TEST_USER -p$DATABASE_TEST_PASS < protected/modules/sample/sql/openeyes_sample_data.sql
 echo "Done."

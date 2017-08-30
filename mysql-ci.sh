b400125ecf9d4bd8fa802eb17d016be54b926044
Andrew Pigram <andrew.pigram@fivium.com.au>
2017-08-30 17:45:58 +1000

    Attempted to get SQL script working.

--- mysql-ci.sh
+++ mysql-ci.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 echo "Adding openeyes user..."
-mysql -u root
-CREATE USER $DATABASE_TEST_USER@$DATABASE_TEST_HOST IDENTIFIED BY password($DATABASE_TEST_PASS);
+mysql -u root -Bse "CREATE USER '$DATABASE_TEST_USER'@'$DATABASE_TEST_HOST' IDENTIFIED BY password('$DATABASE_TEST_PASS');
+CREATE DATABASE \`$DATABASE_TEST_NAME\`;
 COMMIT;
-GRANT ALL PRIVILEGES ON *.* TO $DATABASE_TEST_USER@$DATABASE_TEST_HOST;
-FLUSH PRIVILEGES;
-exit;
+GRANT ALL PRIVILEGES ON *.* TO '$DATABASE_TEST_USER'@'$DATABASE_TEST_HOST';
+FLUSH PRIVILEGES;"
 echo "Done."
 echo "Importing sample data..."
-mysql -u $DATABASE_TEST_USER -p$DATABASE_TEST_PASS < protected/modules/sample/sql/openeyes_sample_data.sql
+mysql $DATABASE_TEST_NAME -u $DATABASE_TEST_USER -p$DATABASE_TEST_PASS < protected/modules/sample/sql/openeyes_sample_data.sql
 echo "Done."

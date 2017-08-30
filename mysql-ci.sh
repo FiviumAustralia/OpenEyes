a6b059cefa7786d74a373585aefdd4165bf648bc
Andrew Pigram <andrew.pigram@fivium.com.au>
2017-08-30 17:32:24 +1000

    Added echo statements to mysql-ci.sh to indicate where in the script the build process is up to.

--- mysql-ci.sh
+++ mysql-ci.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "Adding openeyes user..."
 mysql -u root
 CREATE USER $DATABASE_TEST_USER@$DATABASE_TEST_HOST IDENTIFIED BY password($DATABASE_TEST_PASS);
 COMMIT;
 GRANT ALL PRIVILEGES ON *.* TO $DATABASE_TEST_USER@$DATABASE_TEST_HOST;
 FLUSH PRIVILEGES;
 exit;
+echo "Done."
+echo "Importing sample data..."
 mysql -u $DATABASE_TEST_USER -p$DATABASE_TEST_PASS < protected/modules/sample/sql/openeyes_sample_data.sql
+echo "Done."

659a558bbf672c53e85c33bd86869338bd5da574
Andrew Pigram <andrew.pigram@fivium.com.au>
2017-08-31 11:30:41 +1000

    Added IDENTIFIED BY clause to GRANT statement. Added MYSQL_ROOT_PASS environment variable that contains the mysql root password.

--- mysql-ci.sh
+++ mysql-ci.sh
@@ -5,12 +5,12 @@ mysql -u root -Bse "CREATE USER '$DATABASE_TEST_USER'@'$DATABASE_TEST_HOST' IDEN
 CREATE DATABASE \`$DATABASE_TEST_NAME\`;
 CREATE DATABASE \`$DATABASE_NAME\`;
 COMMIT;
-GRANT ALL PRIVILEGES ON *.* TO '$DATABASE_TEST_USER'@'$DATABASE_TEST_HOST';
+GRANT ALL PRIVILEGES ON *.* TO '$DATABASE_TEST_USER'@'$DATABASE_TEST_HOST' IDENTIFIED BY '$DATABASE_TEST_PASS';
 FLUSH PRIVILEGES;"
 echo "Done."
 echo "Importing sample data..."
 echo "Importing dev data..."
-mysql $DATABASE_NAME -u root < protected/modules/sample/sql/openeyes_sample_data.sql
+mysql $DATABASE_NAME -u root -p"$MYSQL_ROOT_PASS" < protected/modules/sample/sql/openeyes_sample_data.sql
 echo "Importing test data..."
-mysql $DATABASE_TEST_NAME -u root < protected/modules/sample/sql/openeyes_sample_data.sql
+mysql $DATABASE_TEST_NAME -u root -p"$MYSQL_ROOT_PASS" < protected/modules/sample/sql/openeyes_sample_data.sql
 echo "Done."

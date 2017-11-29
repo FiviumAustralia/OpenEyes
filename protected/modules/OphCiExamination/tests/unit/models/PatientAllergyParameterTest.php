0b638f8c7c50393707b3c0c4d5389dc90024334e
Andrew Pigram <andrew.pigram@fivium.com.au>
2017-11-29 15:55:14 +1100

    Disabled Family History tests temporarily due to ongoing changes with case search. Removed search providers from unit tests and reworked some older unit tests that were referring to removed functions.

--- protected/modules/OphCiExamination/tests/unit/models/PatientAllergyParameterTest.php
+++ protected/modules/OphCiExamination/tests/unit/models/PatientAllergyParameterTest.php
@@ -11,11 +11,6 @@ class PatientAllergyParameterTest extends CDbTestCase
      */
     protected $object;
 
-    /**
-     * @var DBProvider
-     */
-    protected $searchProvider;
-
     protected $fixtures = array(
         'patient' => 'Patient',
         'allergy' => 'Allergy',
@@ -25,78 +20,24 @@ class PatientAllergyParameterTest extends CDbTestCase
         'episode' => 'Episode',
     );
 
+    public function __construct()
+    {
+        parent::__construct();
+        $sql = Yii::app()->db->createCommand('SELECT id FROM patient');
+        $this->universe = array_column($sql->queryAll(), 'id');
+    }
+
     protected function setUp()
     {
         parent::setUp();
         $this->object = new PatientAllergyParameter();
-        $this->searchProvider = new DBProvider('mysql');
         $this->object->id = 0;
     }
 
     protected function tearDown()
     {
         parent::tearDown();
-        unset($this->object, $this->searchProvider);
-    }
-
-    /**
-     * @covers PatientAllergyParameter::query()
-     */
-    public function testQuery()
-    {
-        $this->object->textValue = 5;
-
-        $correctOps = array(
-            '=',
-            '!=',
-        );
-        $invalidOps = array(
-            'NOT LIKE',
-        );
-
-        $sqlValue = 'SELECT DISTINCT p.id 
-FROM patient p 
-LEFT JOIN patient_allergy_assignment paa
-  ON paa.patient_id = p.id
-LEFT JOIN allergy a
-  ON a.id = paa.allergy_id
-WHERE a.name = :p_al_textValue_0';
-
-        // Ensure the query is correct for each operator.
-        foreach ($correctOps as $operator) {
-            $this->object->operation = $operator;
-            if ($operator === '!=')
-            {
-                $sqlValue = "SELECT DISTINCT p1.id
-FROM patient p1
-WHERE p1.id NOT IN (
-  $sqlValue
-)";
-            }
-
-            $this->assertEquals($sqlValue, $this->object->query($this->searchProvider));
-        }
-
-        // Ensure that a HTTP exception is raised if an invalid operation is specified.
-        $this->setExpectedException(CHttpException::class);
-        foreach ($invalidOps as $operator) {
-            $this->object->operation = $operator;
-            $this->object->query($this->searchProvider);
-        }
-    }
-
-    /**
-     * @covers PatientAllergyParameter::bindValues()
-     */
-    public function testBindValues()
-    {
-        $this->object->textValue = 5;
-        $expected = array(
-            'p_al_textValue_0' => $this->object->textValue,
-        );
-
-        // Ensure that all bind values are returned.
-        $this->assertEquals($expected, $this->object->bindValues());
+        unset($this->object);
     }
 
     public function testSearch()
@@ -106,13 +47,9 @@ WHERE p1.id NOT IN (
 
         $expected = array($this->patient('patient1'));
 
-        $results = $this->searchProvider->search(array($this->object));
+        $results = $this->object->getResultSet($this->universe);
 
-        $ids = array();
-        foreach ($results as $result) {
-            $ids[] = $result['id'];
-        }
-        $patients = Patient::model()->findAllByPk($ids);
+        $patients = Patient::model()->findAllByPk($results);
 
         $this->assertEquals($expected, $patients);
 
@@ -124,13 +61,9 @@ WHERE p1.id NOT IN (
             $expected[] = $this->patient("patient$i");
         }
 
-        $results = $this->searchProvider->search(array($this->object));
+        $results = $this->object->getResultSet($this->universe);
 
-        $ids = array();
-        foreach ($results as $result) {
-            $ids[] = $result['id'];
-        }
-        $patients = Patient::model()->findAllByPk($ids);
+        $patients = Patient::model()->findAllByPk($results);
 
         $this->assertEquals($expected, $patients);
     }
